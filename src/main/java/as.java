43a06863c080aeb37393e0741f19ad98cae2dea1
SCFrancisco2221857 <2221857@my.ipleiria.pt>
2024-04-17 16:51:13 +0100

    asd

--- src/main/java/as.java
+++ src/main/java/as.java
@@ -1,5 +1,5 @@
 public class as {
     public static void main(String[] args) {
-        System.out.println("se leste isto es gay");
+        System.out.println("se leste isto es gay !!");
     }
 }
