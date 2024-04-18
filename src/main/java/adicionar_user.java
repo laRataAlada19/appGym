a8823d7cb456313f3c9968f1ff6de19ef40218cf
SCFrancisco2221857 <2221857@my.ipleiria.pt>
2024-04-18 08:42:48 +0100

    fotos

--- src/main/java/adicionar_user.java
+++ src/main/java/adicionar_user.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
 public class adicionar_user {
+    private JPanel painelPrincipal;
+
+    public adicionar_user(JPanel painelPrincipal) {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelPrincipal);
+        pack();
+    }
 }
